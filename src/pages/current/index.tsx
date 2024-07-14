3aeb1502cd6d7595b85e5511afaab3a371c551b6
boomchanotai <boom9387@hotmail.com>
2024-07-14 15:03:08 +0700

    revert ro simple

--- src/pages/current/index.tsx
+++ src/pages/current/index.tsx
@@ -1,44 +1,25 @@
 import moment from "moment";
 import { useQueueData } from "../../hook/queries/queueData";
 import { formatTime } from "../../utils";
-import { useEffect, useState } from "react";
 
 const Index = () => {
   const queueData = useQueueData();
-
-  const [currentQueue, setSurrentQueue] = useState<string[] | undefined>(
-    undefined
-  );
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const current = queueData.data?.values.slice(2).filter((item) => {
-        const startTime = formatTime(item[1]);
-        const endTime = formatTime(item[2]);
-        const currentTime = moment();
-
-        return currentTime.isBetween(startTime, endTime);
-      });
-
-      if (!current) return;
-
-      setSurrentQueue(current[0]);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [queueData.data?.values]);
 
   if (queueData.isFetching) {
     return <div>Loading...</div>;
   }
 
-  if (!currentQueue) {
-    return <div>No current queue</div>;
-  }
+  const currentQueue = queueData.data?.values.slice(2).filter((item) => {
+    const startTime = formatTime(item[1]);
+    const endTime = formatTime(item[2]);
+    const currentTime = moment();
+
+    return currentTime.isBetween(startTime, endTime);
+  });
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center border-b border-black">
-      {currentQueue && (
+      {currentQueue && currentQueue?.length > 0 && (
         <div className="flex flex-col items-center justify-center gap-6">
           <div className="text-gray-500 font-bold">Current Team</div>
           <div className="font-bold text-7xl">{currentQueue[0][6]}</div>
