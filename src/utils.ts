9f27ef45c597606c261d5cde80761d5b2a948811
boomchanotai <boom9387@hotmail.com>
2024-07-14 14:58:58 +0700

    fix: eiei

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import moment from "moment";
 
 export const formatTime = (time: string) => {
+  if (!time) return moment();
   const splitTime = time.split(":");
   return moment()
     .startOf("day")
